58f9dcc9e76f36cac9dad4ea3256e8bebe26064a
12Zholdoshbek <zholdsohbektashiev@gmail.com>
2025-05-19 14:37:33 +0600

    "Initial commit"

---
+++ src/main/java/com/tashiev/realtorhub/RealtorHubApplication.java
@@ -0,0 +1,13 @@
+package com.tashiev.realtorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtorHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtorHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tashiev/realtorhub/RealtorHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tashiev.realtorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtorHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
